fe04c07205dc61d4b362e4dada21489394d1e0ef
Jaime Aquino Martinez <jhonesbortoni@gmail.com>
2025-11-24 18:48:06 -0600

    aea

--- ClaseDAPPS/index.js
+++ ClaseDAPPS/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const Cors = require('cors');
 
 //const userRoutes = require('./routes/user');
 const pagosRoutes = require('./routes/pagos');
@@ -8,10 +9,12 @@ const walletRoutes = require('./routes/wallet');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.json());
+app.use(Cors());
 
 //app.use('/api/user', userRoutes);
 app.use('/api/wallet', walletRoutes);
 app.use('/api/pagos', pagosRoutes);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server at port ${PORT}`));
